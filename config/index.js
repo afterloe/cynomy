e5d0420e94d5912369929ff30bc1c6b0a4741288
afterloe <lm6289511@gmail.com>
2017-01-03 19:01:03 +0800

    命令行实现创建js功能

--- config/index.js
+++ config/index.js
@@ -10,8 +10,12 @@
 "use strict";
 
 const {resolve} = require("path");
-const [configRoule, pkg] = [new Map(), require(resolve(__dirname, "..", "package"))];
+const [configRoule, pkg, cfg] = [new Map(), require(resolve(__dirname, "..", "package")), require(resolve(__dirname, "..", "configuration.json"))];
 
+configRoule.set("author", cfg.author);
+configRoule.set("licensed", cfg.licensed);
+configRoule.set("mail", cfg.mail);
+configRoule.set("homePage", cfg.homePage);
 configRoule.set("version", pkg.version);
 
 const get = key => configRoule.has(key)? configRoule.get(key) : null;
