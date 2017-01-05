ac47d0618232c0815064d0a36c1a9137f626b15c
afterloe <lm6289511@gmail.com>
2017-01-05 14:53:47 +0800

    cynomy 支持项目配置和全局配置 .cynomy配置文件

--- config/index.js
+++ config/index.js
@@ -10,16 +10,34 @@
 "use strict";
 
 const {resolve} = require("path");
-const [configRoule, pkg, cfg] = [new Map(), require(resolve(__dirname, "..", "package")), require(resolve(__dirname, "..", "configuration.json"))];
+const [configRoule, pkg] = [new Map(), require(resolve(__dirname, "..", "package"))];
 
-configRoule.set("author", cfg.author); // 设置作者信息
-configRoule.set("licensed", cfg.licensed); // 设置开源协议信息
-configRoule.set("mail", cfg.mail); // 设置邮件信息
-configRoule.set("homePage", cfg.homePage); // 设置首页信息
 configRoule.set("version", pkg.version); // 设置版本信息
 
+/**
+ * 获取配置
+ *
+ * @param  {[String]} key [配置项]
+ * @return {[String]}     [配置内容]
+ */
 const get = key => configRoule.has(key)? configRoule.get(key) : null;
+
+/**
+ * 设置客制化内容
+ *
+ * @param {[String]} author   [开发者]
+ * @param {[String]} licensed [开源协议]
+ * @param {[String]} mail     [开发者邮箱]
+ * @param {[String]} homePage [开发者web页]
+ */
+const setIndividualization = ({author = process.env.LOGNAME, licensed = "", mail = "", homePage = ""}) => {
+		configRoule.set("author", author); // 设置作者信息
+		configRoule.set("licensed", licensed); // 设置开源协议信息
+		configRoule.set("mail", mail); // 设置邮件信息
+		configRoule.set("homePage", homePage); // 设置首页信息
+};
 
 module.exports = {
 	get,
+	setIndividualization,
 };
